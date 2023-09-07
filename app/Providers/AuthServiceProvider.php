90bce9f51dd9caba0a2972e89586e54655f012b3
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-09-07 12:52:28 +0700

    bring on the code

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public static $permission = [
+        'dashboard' => ['superadmin', 'admin'],
+        'user-index' => ['admin'],
+        'payment-update' => ['superadmin'],
+    ];
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        foreach (Self::$permission as $features => $roles) {
+            Gate::define($features, function(User $user) use ($roles) {
+                if (in_array($user->role, $roles)) {
+                    return true;
+                }
+            });
+        }
+        
+    }
+}
